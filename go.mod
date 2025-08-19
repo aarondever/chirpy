2a21d9533803a86f69fcb9324f9222a4e4eaafba
Aaron <33992792+aarondever@users.noreply.github.com>
2025-08-19 15:32:38 +0800

    routing: stateful handlers

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/aarondever/chirpy
+
+go 1.25.0
---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+)
+
+func main() {
+	cfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+
+	serverMux := http.NewServeMux()
+
+	serverMux.Handle("/app/",
+		cfg.middlewareMetricsInt(
+			http.StripPrefix("/app",
+				http.FileServer(http.Dir(".")))))
+	serverMux.HandleFunc("/healthz", handleReadiness)
+	serverMux.HandleFunc("/metrics", cfg.handleMetrics)
+	serverMux.HandleFunc("/reset", cfg.resetMetrics)
+
+	server := http.Server{
+		Handler: serverMux,
+		Addr:    ":8080",
+	}
+
+	server.ListenAndServe()
+}
+
+func handleReadiness(responseWriter http.ResponseWriter, request *http.Request) {
+	responseWriter.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	responseWriter.WriteHeader(http.StatusOK)
+
+	body := []byte("OK")
+	responseWriter.Write(body)
+}
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInt(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handleMetrics(responseWriter http.ResponseWriter, request *http.Request) {
+	body := fmt.Appendf(nil, "Hits: %d", cfg.fileserverHits.Load())
+	responseWriter.Write(body)
+}
+
+func (cfg *apiConfig) resetMetrics(responseWriter http.ResponseWriter, request *http.Request) {
+	cfg.fileserverHits.Store(0)
+}
